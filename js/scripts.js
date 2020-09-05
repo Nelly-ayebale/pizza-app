65396e8856666e86af95346aac4df27e9d609e8a
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-09-05 16:33:08 +0300

    Created a prototype for pizza

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ function Pizza(size, crust, toppings) {
     this.crust = crust;
     this.toppings = toppings;
 }
+Pizza.prototype.pizzaType = function () {
+    return this.size + " " + this.crust + "" + this.toppings;
+}
+
+$(document).ready(function () {
+
+})
