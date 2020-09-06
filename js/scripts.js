44b30fda710da80516891e2faee1f430c8a38f64
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-09-06 20:54:55 +0300

    Made other pizza prototypes for the size, crust and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ function Pizza(type, size, crust, toppings) {
 }
 
 Pizza.prototype.pizzaPrice = function () {
-    return this.SizePrice() + this.CrustPrice() + this.ToppingsPrice();
+    return this.sizePrice() + this.crustPrice() + this.toppingsPrice();
 }
-Pizza.prototype.getToppingsPrice() = function () {
+Pizza.prototype.toppingsPrice() = function () {
     if (this.size === "large")
         if (this.toppings === "pepperoni") {
             return 7000
@@ -21,6 +21,71 @@ Pizza.prototype.getToppingsPrice() = function () {
         } else {
             return 4000
         }
+    else if (this.size === "medium")
+        if (this.toppings === "pepperoni") {
+            return 5000
+        } else if (this.toppings === "mushrooms") {
+            return 4500
+        } else if (this.toppings === "bacon") {
+            return 4500
+        } else if (this.toppings === "cheese") {
+            return 5000
+        } else {
+            return 4000
+        }
+    else {
+
+        if (this.toppings === "pepperoni") {
+            return 4000
+        } else if (this.toppings === "mushrooms") {
+            return 3500
+        } else if (this.toppings === "bacon") {
+            return 3500
+        } else if (this.toppings === "cheese") {
+            return 4000
+        } else {
+            return 2000
+        }
+    }
+
+
+}
+Pizza.prototype.sizePrice() = function () {
+    if (this.size === "large") {
+        return 9000
+    } else if (this.size === "medium") {
+        return 7000
+    } else {
+        return 5000
+    }
+}
+Pizza.prototype.crustPrice() = function () {
+    if (this.size === "large")
+        if (this.crust === "crispy") {
+            return 3000
+        } else if (this.crust === "stuffed") {
+            return 4000
+        } else {
+            return 2000
+        }
+    else if (this.size === "medium") {
+        if (this.crust === "crispy") {
+            return 2500
+        } else if (this.crust === "stuffed") {
+            return 3000
+        } else {
+            return 1500
+        }
+    }
+    else {
+        if (this.crust === "crispy") {
+            return 2000
+        } else if (this.crust === "stuffed") {
+            return 2500
+        } else {
+            return 1000
+        }
+    }
 
 
 }
